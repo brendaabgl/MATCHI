7aaba907680553204ab1f871c9a60755bb830557
briggea <geasandhea19@gmail.com>
2024-12-06 01:39:00 +0700

    cleaning up merge

--- public/controller/controller.js
+++ public/controller/controller.js
@@ -48,6 +48,37 @@ function goToProfile() {
     }
 }
 
+document.getElementById('contactForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert("Thank you for submitting the form!");
+});
+
+function toggleSearch() {
+    const searchBox = document.getElementById('search-box');
+    searchBox.classList.toggle('d-none');
+}
+
+//newsletter
+const submitButton = document.getElementById('submitBtn');
+const emailInput = document.getElementById('emailInput');
+const responseMessage = document.getElementById('responseMessage');
+const errorMessage = document.getElementById('errorMessage');
+
+submitButton.addEventListener('click', function() {
+    const email = emailInput.value;
+    const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+
+    if (emailPattern.test(email)) {
+        responseMessage.style.display = 'block';
+        errorMessage.style.display = 'none';
+        emailInput.value = '';  
+    } else {
+        errorMessage.style.display = 'block';
+        responseMessage.style.display = 'none';
+    }
+});
+
+
 var app = angular.module('myApp', []);
 
 app.controller('TextileController', function($scope, $http) { 
